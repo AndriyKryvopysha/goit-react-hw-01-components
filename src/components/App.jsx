29f2dec1efe052cc2ae2fe283d0a5063dcd5ccaa
Andriy Kryvopysha <logan1kry@gmail.com>
2022-05-25 18:28:12 +0300

    Task 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Container } from './Container/Container';
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 
+import { Statistics } from './Statistics/Statistics';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -12,7 +14,9 @@ export const App = () => {
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-    />
+      />;
+      <Statistics title="Upload stats" stats={data} />;
+      
   </Container>
         
   );
